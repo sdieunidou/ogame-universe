c37383900f6402921034523eae348e8ed5e904fb
Seb <s.dieunidou@gmail.com>
2021-03-14 15:52:42 +0100

    fix security

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,7 +22,6 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
@@ -33,7 +32,6 @@ class RegistrationController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_home');
         }
