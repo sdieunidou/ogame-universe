6e8c2322f335cc651ed03b1bf625efff62a26cc1
Seb <s.dieunidou@gmail.com>
2021-03-14 15:57:34 +0100

    update

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,6 +17,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
